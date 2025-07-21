45f2be77b7e724f7020f66640c6e6c142d81ae08
AliBakhshi274 <alibakhshi.bs@gmail.com>
2025-07-21 23:38:59 +0330

    Add Task model with user association and admin registration

--- core/todo/models.py
+++ core/todo/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from accounts.models import Profile
+class Task(models.Model):
+    '''
+    Create Task model
+    '''
+    user = models.ForeignKey(Profile, on_delete=models.CASCADE, null=True, blank=True)
+    title = models.CharField(max_length=100)
+    complete = models.BooleanField(default=False)
 
-# Create your models here.
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        order_with_respect_to = "user"
